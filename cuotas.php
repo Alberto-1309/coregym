f9528d0e6e1173345131b670e70d2d4733bf44db
alberto-1309 <alberto.melladomontes@alum.uca.es>
2024-03-19 17:57:46 +0100

    añadido ficheros

--- cuotas.php
+++ cuotas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coregym</title>
+    <title>Cuotas</title>
     <!-- CSS GENERAL -->
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="css/header.css">
